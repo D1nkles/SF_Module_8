621d7000cc6c1b1a8de07d92b9c8ed1492c7e792
Ilia <isinetov@mail.ru>
2024-05-02 19:55:47 +0400

    Задание 8.3.1. Исходный код программы — ещё один отличный пример текстового файла.
    Напишите программу, которая выводит свой собственный исходный код в консоль.

---
+++ Task 8_3_1/Task 8_3_1.cs
@@ -0,0 +1,9 @@
+﻿string codePATH = @"C:\Users\Dinkles\source\repos\SkillfactoryModules\SF_Module_8\Task 8_3_1\Task 8_3_1.cs";
+using (StreamReader sr = File.OpenText(codePATH)) 
+{
+    string str;
+    while ((str = sr.ReadLine()) != null) 
+    {
+        Console.WriteLine(str);
+    }
+}

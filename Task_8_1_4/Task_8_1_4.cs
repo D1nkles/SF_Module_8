504843e87427533d331a1571ea5a63fe6a3cdd79
Ilia <isinetov@mail.ru>
2024-05-02 14:03:24 +0400

    Добавьте файлы проекта.

---
+++ Task_8_1_4/Task_8_1_4.cs
@@ -0,0 +1,30 @@
+﻿class DiskManager 
+{
+    private string name;
+	public string Name
+	{
+		get { return name; }
+		set { name = value; }
+	}
+
+	private decimal space;
+    public decimal Space
+    {
+        get { return space; }
+        set { space = value; }
+    }
+
+    private decimal freeSpace;
+    public decimal FreeSpace
+    {
+        get { return freeSpace; }
+        set { freeSpace = value; }
+    }
+
+    DiskManager(string name, decimal space, decimal freeSpace) 
+    {
+        Name = name;
+        Space = space;
+        FreeSpace = freeSpace;
+    }
+}

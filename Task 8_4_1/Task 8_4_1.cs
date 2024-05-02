e658fdb6777798716ac87ac748ebcc18de982903
Ilia <isinetov@mail.ru>
2024-05-02 22:16:26 +0400

    Задание 8.4.2. Запишите  в файл из предыдущего задания информацию о доступе к нему с вашей машины.

---
+++ Task 8_4_1/Task 8_4_1.cs
@@ -0,0 +1,15 @@
+﻿FileInfo binaryFile = new FileInfo(@"C:\Users\Dinkles\Desktop\BinaryFile.bin");
+
+using (BinaryWriter bw = new BinaryWriter(binaryFile.OpenWrite()))
+{
+    bw.Write($"Файл изменен {DateTime.Now} на компьютере {Environment.OSVersion}");
+}
+
+string str;
+using (BinaryReader br = new BinaryReader(binaryFile.OpenRead())) 
+{
+    str = br.ReadString();
+}
+
+
+Console.WriteLine(str);

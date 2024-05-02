405a11c4ec70458f51d433e36165b9863cff556a
Ilia <isinetov@mail.ru>
2024-05-02 20:52:24 +0400

    Задание 8.3.2. Сделайте так, чтобы ваша программа из задания 8.3.1 при каждом запуске добавляла в свой исходный код комментарий о времени последнего запуска.

--- Task 8_3_1/Task 8_3_1.cs
+++ Task 8_3_1/Task 8_3_1.cs
@@ -1,5 +1,11 @@
-﻿string codePATH = @"C:\Users\Dinkles\source\repos\SkillfactoryModules\SF_Module_8\Task 8_3_1\Task 8_3_1.cs";
-using (StreamReader sr = File.OpenText(codePATH)) 
+﻿FileInfo code = new FileInfo(@"C:\Users\Dinkles\source\repos\SkillfactoryModules\SF_Module_8\Task 8_3_1\Task 8_3_1.cs");
+
+using (var a = code.AppendText()) 
+{ 
+    
+    a.WriteLine($"//Время последнего заупуска: {DateTime.Now}");
+}
+using (StreamReader sr = File.OpenText(code.FullName))
 {
     string str;
     while ((str = sr.ReadLine()) != null) 

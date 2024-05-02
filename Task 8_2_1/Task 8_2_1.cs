e3aaec2eedda084f81a111405c3b65b1fe248118
Ilia <isinetov@mail.ru>
2024-05-02 16:47:57 +0400

    Задание 8.2.2 Добавьте в метод из задания 8.2.1 создание новой директории в корне вашего диска, а после вновь выведите количество элементов уже после создания нового.

--- Task 8_2_1/Task 8_2_1.cs
+++ Task 8_2_1/Task 8_2_1.cs
@@ -1,9 +1,11 @@
 ﻿try
 {
     DirectoryInfo root = new DirectoryInfo(@"C:\\");
-
+    DirectoryInfo newDirectory = new DirectoryInfo(@"C:\\NewFolder\\");
     if (root.Exists)
     {
+        Console.WriteLine($"Кол-во объектов в корне диска:  {root.GetDirectories().Length + root.GetFiles().Length}");
+        newDirectory.Create();
         Console.WriteLine($"Кол-во объектов в корне диска:  {root.GetDirectories().Length + root.GetFiles().Length}");
     }
 }

147b7bbc6a4cf5b7285f45238f0541cb06e3b547
Ilia <isinetov@mail.ru>
2024-05-02 17:29:39 +0400

    Задание 8.2.4 Создайте на рабочем столе папку testFolder. Напишите метод, с помощью которого можно будет переместить её в корзину.

---
+++ Task 8_2_4/Task 8_2_4.cs
@@ -0,0 +1,10 @@
+﻿DirectoryInfo folder = new DirectoryInfo(@"C:\Users\Dinkles\Desktop\testFolder");
+
+DirectoryInfo bin = new DirectoryInfo(@"C:\$Recycle.Bin");
+
+void MoveToBin(DirectoryInfo folder) 
+{
+    folder.MoveTo(@$"{bin.FullName}\testFolder");
+}
+
+MoveToBin(folder);
